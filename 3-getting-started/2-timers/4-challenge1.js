5fc37515c4a4b0bf3878535b8badb91798f326ee
Liam <81374620+LiamMCode@users.noreply.github.com>
2021-06-03 14:07:44 +0100

    section 3 tasks completed

--- 3-getting-started/2-timers/4-challenge1.js
+++ 3-getting-started/2-timers/4-challenge1.js
@@ -2,6 +2,14 @@ const theOneFunc = () => {};
 
 setTimeout(theOneFunc, 4 * 1000);
 
+const delay = who => {
+    console.log(who + ' delay' + ' seconds');
+  };
+  
+  setTimeout(delay, 4 * 1000, 4);
+  setTimeout(delay, 8 * 1000, 4);
+  
+
 // Hello after 4 seconds
 
 // Hello after 8 seconds
